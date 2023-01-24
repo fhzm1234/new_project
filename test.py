99acf1e89d2cf791db4c30111a50488f27181cd9
fhzm1234 <djshf466@naver.com>
2023-01-24 23:37:26 +0900

    calc

--- test.py
+++ test.py
@@ -1,2 +1,23 @@
-for i in range(1, 101):
-    print(i)
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    return a / b
+
+a = 0
+a = int(input())
+
+b = 0
+b = int(input())
+
+print("add :", add(a, b))
+print("sub :", sub(a, b))
+print("mul :", mul(a, b))
+print("div :", div(a, b))
+
